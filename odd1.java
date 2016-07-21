18e8ce93bcbcfc072c5182fbab1b583e516aceca
Abirami16 <abiramibalu16@gmail.com>
2016-07-22 05:00:28 +0530

    hh
    hh

---
+++ odd1.java
@@ -0,0 +1,18 @@
+class Odd
+{
+public static void main(Sting []args)
+{
+int input=Integer.parseInt(args[0]);
+if(input<0)
+{
+System.out.println("the given number is negative");
+}
+else if(input==0)
+{
+System.out.println("the number is neither positive nor negative");
+}
+else{
+System.out.println("the number is positive");
+}
+}
+}
