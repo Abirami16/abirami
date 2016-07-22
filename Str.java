f292d491f4f0f541d4bf24cc3b4d9eb6e632f0f4
Abirami16 <abiramibalu16@gmail.com>
2016-07-22 05:40:13 +0530

    Create Str.java

---
+++ Str.java
@@ -0,0 +1,19 @@
+class Hello
+{
+public static void main(String []args)
+{
+int num=Integer.parseInt(args[0]);
+if(num==5)
+{
+for(int i=0;i<5;i++)
+{
+System.out.println("Hello");
+}
+}
+else
+{
+System.out.println("0");
+}
+}
+}
+
